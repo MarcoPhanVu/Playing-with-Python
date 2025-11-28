7bbd6c2efd19851aabdf57fc04a4cb89718c3b60
Ben Phan <88922348+MarcoPhanVu@users.noreply.github.com>
2025-11-28 02:05:47 -0800

    privated some functions

--- minesweeper.py
+++ minesweeper.py
@@ -5,7 +5,7 @@ from tkinter import *
 # Define what will cell behave when rightclick
 def cell_flagged_triggered(event):
 	event.widget.configure(bg="red")
-	print(event.winfo_children())
+	event.widget.configure(bg="red")
 	print(type(event))
 	print("cell flagged")
 
@@ -66,7 +66,7 @@ class MineSweeper:
 
 		self.display_board = [[self.emptyCellSign for cell in range(width)] for cell in range(height)]
 
-		self.generate_actual_board(self.width, self.height, self.bombs)
+		self.__generate_actual_board(self.width, self.height, self.bombs)
 
 		# Visualize Boards for display and actual
 		for i in range(self.height):
@@ -80,36 +80,36 @@ class MineSweeper:
 				self.display_board_frame.columnconfigure(j, weight = 1)
 				self.actual_board_frame.columnconfigure(j, weight = 1)
 
-				cell = tk.Button(	self.display_board_frame,
-									# text = f"{j} d",
-									text = " ",
-									width = 3,
-									font = self.fontDefault,
-									command = lambda i = i, j = j: 
-									self.pick(i, j, self.display_board))
-				cell.bind("<Button-3>", cell_flagged_triggered)
-				cell.grid(row = i, column = j, sticky = "NSEW")
+				display_cell = tk.Button(	self.display_board_frame,
+											text = " ",
+											width = 3,
+											font = self.fontDefault,
+											command = lambda i = i, j = j: 
+											self.pick(i, j, self.display_board))
+				display_cell.bind("<Button-3>", cell_flagged_triggered)
+				display_cell.value = "E" #empty
+				display_cell.grid(row = i, column = j, sticky = "NSEW")
 				# , padx = 1, pady = 1)
-				self.display_rows.append(cell)
+				self.display_rows.append(display_cell)
 
-				cell = tk.Button(	self.actual_board_frame,
-									# text = f"{j} a",
-									text = f"{self.actual_board[i][j]}",
-									width = 3,
-									font = self.fontDefault,
-									command = lambda i = i, j = j: 
-									self.pick(i, j, self.actual_board))
-				cell.bind("<Button-3>", cell_flagged_triggered)
-				cell.grid(row = i, column = j, sticky = "NSEW")
+				actual_cell = tk.Button(	self.actual_board_frame,
+											text = f"{self.actual_board[i][j]}",
+											width = 3,
+											font = self.fontDefault,
+											command = lambda i = i, j = j: 
+											self.pick(i, j, self.actual_board))
+				actual_cell.bind("<Button-3>", cell_flagged_triggered)
+				actual_cell.value = self.actual_board[i][j]
+				actual_cell.grid(row = i, column = j, sticky = "NSEW")
 				# , padx = 1, pady = 1)
-				self.actual_rows.append(cell)
+				self.actual_rows.append(actual_cell)
 
 			self.display_cells.append(self.display_rows)
 			self.actual_cells.append(self.actual_rows)
 
 		self.root.mainloop()
 
-	def generate_actual_board(self, width = 8, height = 8, bombs = 5):
+	def __generate_actual_board(self, width = 8, height = 8, bombs = 5):
 		board = []
 		bomb_locs = self.__generate_bombs(width, height, bombs)
 
@@ -122,9 +122,9 @@ class MineSweeper:
 					board[i].append(0)
 		self.actual_board = board
 
-		self.generate_cell_num()
+		self.__generate_cell_num()
 
-	def __generate_bombs(self, width, height, bombs):
+	def __generate_bombs(self, width, height, bombs): # private function(only functions inside Minesweeper Class can call and use this)
 		#randomize bomb locs 1D array
 		bomb_loc_1D = []
 		temp = 0
@@ -150,12 +150,12 @@ class MineSweeper:
 			print(f"Cell at [{x}][{y}] revealed already\n")
 			print(f"Show surrnding cells around Cell:[{x}][{y}]\n")
 
-			# # Show surronding cells
-			# for i in range(-1, 2): # to include 1
-			# 	for j in [-1, 0, 1]: # same as above
-			# 		new_x = i + x
-			# 		new_y = j + y
-			# 		self.pick(new_x, new_y, board_in_general)
+			# Show surronding cells
+			for i in range(-1, 2): # to include 1
+				for j in [-1, 0, 1]: # same as above
+					new_x = i + x
+					new_y = j + y
+					self.pick(new_x, new_y, board_in_general)
 			return
 		
 		if self.actual_board[x][y] != 0:
@@ -163,9 +163,9 @@ class MineSweeper:
 
 		# Flood reveal -> turn this to be a recursive function
 		if self.actual_board[x][y] == 0:
-			self.flood_reveal(x, y)
+			self.__flood_reveal(x, y)
 
-		self.update_display_board()
+		self.__update_display_board()
 		#debug session
 		if board_in_general == self.actual_board:
 			print("Picking from Actual Board")
@@ -177,7 +177,7 @@ class MineSweeper:
 		except IndexError as e:
 			print(f"IndexErr(pick:): [{x}][{y}] is out of range\n")
 
-	def flood_reveal(self, x, y): # Recursive function
+	def __flood_reveal(self, x, y): # Recursive function
 		self.display_board[x][y] = 0
 
 		# Check neighbors -> 2 ways
@@ -204,26 +204,31 @@ class MineSweeper:
 						self.pick(new_x, new_y, self.display_board)
 		print()
 
-	def update_display_board(self):
+	def __update_display_board(self):
 		for i in range(self.height):
 			for j in range(self.width):
+				font_color = "#eeeeee"
+				cell_color = "#9e49c0"
+
 				cell_value = self.display_board[i][j]
 				updated_text = cell_value
-				font_color = "#fff"
-				cell_color = "#eeeeee"
+
+				if cell_value == "F":
+					updated_text = "🚩"
 
 				if cell_value == "B":
 					cell_color = self.BOMBCellColor
 				elif cell_value != self.emptyCellSign:
 					# print(f"Cell Value: {cell_value} at Y{i}, X{j}")
 					cell_color = self.numberedCellColor[cell_value]
+				else:
+					font_color = "#eeeeee"
 					
-
 				cells = self.display_cells[i][j]
 				cells.config(text = updated_text, fg = font_color, bg = cell_color)
 
 
-	def generate_cell_num(self):
+	def __generate_cell_num(self):
 		for x in range(self.height):
 			for y in range(self.width):
 				if self.actual_board[x][y] != "B":

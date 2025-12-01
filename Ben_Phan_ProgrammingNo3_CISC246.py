c72af2836d681380bb01abd6ab9c05335033d738
Ben Phan <88922348+MarcoPhanVu@users.noreply.github.com>
2025-12-01 12:42:21 -0800

    add task 3 into ProAss3

--- Ben_Phan_ProgrammingNo3_CISC246.py
+++ Ben_Phan_ProgrammingNo3_CISC246.py
@@ -50,11 +50,41 @@ def gamblers_expected_time(k: int, M: int, p: float, NUM_SIM: int) -> int:
 	return avg
 
 def run_of_success(p: float, S: int, F: int, NUM_SIM: int) -> float:
-	pass
+	prob = [p, 1 - p]
+	choices = [1, 0]
+
+	S_count = 0
+	F_count = 0
+
+	S_First = 0
+	F_First = 0
+
+	for i in range(NUM_SIM):
+		S_count = 0
+		F_count = 0
+		while (S_count != S) and (F_count != F):
+			res = random.choices(choices, prob)[0]
+			if (res == 1):
+				S_count += 1
+				F_count = 0
+			elif (res == 0):
+				F_count += 1
+				S_count = 0
+		if (S_count == S):
+			S_First += 1
+		if (F_count == F):
+			F_First += 1
+
+	print(f"S_first: {S_First}")
+	print(f"F_first: {F_First}")
+	return (float(S_First) / float(NUM_SIM))
+	
+
 
 if __name__ == "__main__":
 	# print(f"Task 1: {sim_diff_integers(20, 4, 10000)}")
-	# print(f"Task 2: {gamblers_expected_time(10, 200, 0.8, 10000)}")
+	# print(f"Task 2: {gamblers_expected_time(10, 200, 0.8, 10000)} times")
+	print(f"Task 3: {run_of_success(0.7, 10, 5, 10000)} times")
 	print("mains")
 
 		

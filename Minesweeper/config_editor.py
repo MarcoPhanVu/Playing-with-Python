8fc6798a44d554bee62fde26ec97da0c4a9261ca
Ben Phan <88922348+MarcoPhanVu@users.noreply.github.com>
2025-12-05 14:18:17 -0800

    reading and writing json

--- Minesweeper/config_editor.py
+++ Minesweeper/config_editor.py
@@ -1,4 +1,36 @@
 import random
 import math
+import json
+import os
 import tkinter as tk
 from tkinter import *
+
+class Configurator:
+
+
+    def __init__(self):
+        pass
+
+
+# VSC -> root starts at project's workspace
+file = open("./data/highscores_org.txt")
+
+highscores_JSON = {}
+
+
+for line in file:
+    print(line)
+    name = line.split(": ")[0]
+    score = line.split(": ")[1][:-2]
+    highscores_JSON[name] = score
+
+print(highscores_JSON)
+
+
+# json.loads -> to dict
+# json.dumps -> to json
+
+# def main():
+#     pass
+
+# main()
